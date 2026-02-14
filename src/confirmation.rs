1a475141ddd9d132b71f97da386307256c5885c3
George Belden <beldenge@gmail.com>
2026-02-14 11:31:51 -0500

    test(confirmation): add enum sanity coverage

--- src/confirmation.rs
+++ src/confirmation.rs
@@ -3,3 +3,18 @@ pub enum ConfirmationOperation {
     File,
     Bash,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::ConfirmationOperation;
+
+    #[test]
+    fn operations_are_distinct_and_debuggable() {
+        assert_ne!(
+            format!("{:?}", ConfirmationOperation::File),
+            format!("{:?}", ConfirmationOperation::Bash)
+        );
+        assert_eq!(format!("{:?}", ConfirmationOperation::File), "File");
+        assert_eq!(format!("{:?}", ConfirmationOperation::Bash), "Bash");
+    }
+}
